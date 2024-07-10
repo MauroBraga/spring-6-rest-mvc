9e1d15000aa4f0ecf9e328b705cbfc35f543fd0a
mauro <mrb0305@gmail.com>
2024-07-10 09:11:54 -0300

    Logging

--- src/main/java/br/com/mrb/boot/spring_6_rest_mvc/services/BeerServiceImpl.java
+++ src/main/java/br/com/mrb/boot/spring_6_rest_mvc/services/BeerServiceImpl.java
@@ -2,14 +2,20 @@ package br.com.mrb.boot.spring_6_rest_mvc.services;
 
 import br.com.mrb.boot.spring_6_rest_mvc.model.Beer;
 import br.com.mrb.boot.spring_6_rest_mvc.model.BeerStyle;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-public class BeerServiceImpl implements BeerService{
+@Slf4j
+@Service
+public class BeerServiceImpl implements BeerService {
     @Override
     public Beer getBeer(UUID id) {
+        log.debug("Get Beer Id in service was called");
+
         return Beer.builder().id(id)
                 .version(1)
                 .beerName("Galaxy Cat")
