2da9a2a5dc52d5758f3f8ecfbe0d5cf1d7ad974a
mauro <mrb0305@gmail.com>
2024-07-11 09:37:31 -0300

    Logging

--- src/main/java/br/com/mrb/boot/spring_6_rest_mvc/controller/BeerController.java
+++ src/main/java/br/com/mrb/boot/spring_6_rest_mvc/controller/BeerController.java
@@ -5,18 +5,28 @@ import br.com.mrb.boot.spring_6_rest_mvc.services.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 
 @Slf4j
 @AllArgsConstructor
-@Controller
+@RestController
 public class BeerController {
 
     private final BeerService beerService;
 
+    @RequestMapping("/api/v1/beer")
+    public List<Beer> listBeers(){
+        return beerService.listBeers();
+    }
+
     public Beer getBeerById(UUID id){
-        log.debug("Get Beer by id Controller");
-        return beerService.getBeer(id);
+
+        log.debug("Get Beer by Id - in controller");
+
+        return beerService.getBeerById(id);
     }
 }
--- src/main/java/br/com/mrb/boot/spring_6_rest_mvc/services/BeerService.java
+++ src/main/java/br/com/mrb/boot/spring_6_rest_mvc/services/BeerService.java
@@ -2,8 +2,11 @@ package br.com.mrb.boot.spring_6_rest_mvc.services;
 
 import br.com.mrb.boot.spring_6_rest_mvc.model.Beer;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BeerService {
-    Beer getBeer(UUID id);
+    List<Beer> listBeers();
+
+    Beer getBeerById(UUID id);
 }
