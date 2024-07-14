26f83a15f878f7aa26b3633e2467739e486292a0
mauro <mrb0305@gmail.com>
2024-07-14 11:09:55 -0300

    handlePost

--- src/main/java/br/com/mrb/boot/spring_6_rest_mvc/controller/BeerController.java
+++ src/main/java/br/com/mrb/boot/spring_6_rest_mvc/controller/BeerController.java
@@ -4,12 +4,9 @@ import br.com.mrb.boot.spring_6_rest_mvc.model.Beer;
 import br.com.mrb.boot.spring_6_rest_mvc.services.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -21,6 +18,13 @@ import java.util.UUID;
 public class BeerController {
 
     private final BeerService beerService;
+
+    @PostMapping
+    public ResponseEntity handlePost(@RequestBody  Beer beer){
+        Beer savedBeer = beerService.saveNewBeer(beer);
+
+        return  new ResponseEntity(HttpStatus.CREATED);
+    }
 
     @RequestMapping(method = RequestMethod.GET)
     public List<Beer> listBeers(){
